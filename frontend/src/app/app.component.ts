98a73d6a4b5fc7d40e63802c704ebeec68a8fc17
Earl006 <earljoe06@gmail.com>
2025-02-04 12:28:45 +0300

    [Added]: learner/footer component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./learner/navbar/navbar.component";
 import { HeroComponent } from "./learner/hero/hero.component";
+import { FooterComponent } from "./learner/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
